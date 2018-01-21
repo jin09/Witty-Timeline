856415b8bfe740b718e535b885be852196c062c4
Gautam Jain <supergautam995@gmail.com>
2018-01-22 02:09:08 +0530

    gif-response quiz

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,12 +1,7 @@
-self.addEventListener('fetch', function (event) {
-    // TODO: respond to all requests with an html response
-    // containing an element with class="a-winner-is-me".
-    // Ensure the Content-Type of the response is "text/html"
-
-    event.respondWith(
-        new Response('<b class="a-winner-is-me">Hello World!!</b>', {
-            headers: {'Content-Type': 'text/html'}
-        })
-    );
-
+self.addEventListener('fetch', function(event) {
+  // TODO: only respond to requests with a
+  // url ending in ".jpg"
+  event.respondWith(
+    fetch('/imgs/dr-evil.gif')
+  );
 });
