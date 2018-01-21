0e09daacbc9c850fa8bd6fde79e07435b21d7401
Gautam Jain <supergautam995@gmail.com>
2018-01-21 23:40:43 +0530

    register service worker and log requests that go through the service worker

--- public/js/main/IndexController.js
+++ public/js/main/IndexController.js
@@ -8,7 +8,17 @@ export default function IndexController(container) {
   this._toastsView = new ToastsView(this._container);
   this._lostConnectionToast = null;
   this._openSocket();
+  this._registerServiceWorker();
 }
+
+IndexController.prototype._registerServiceWorker = function() {
+  // TODO: register service worker
+  navigator.serviceWorker.register('sw.js').then(function (reg) {
+    console.log('Yay!');
+  }).catch(function () {
+    console.log('Boo!');
+  });
+};
 
 // open a connection to the server for live updates
 IndexController.prototype._openSocket = function() {
