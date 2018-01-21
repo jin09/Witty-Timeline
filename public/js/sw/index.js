82c9212a6e2788104f7783a62265d304980b89a4
Gautam Jain <supergautam995@gmail.com>
2018-01-22 01:56:30 +0530

    answer, response with correct headers

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,6 +1,12 @@
-self.addEventListener('fetch', function(event) {
-	// TODO: respond to all requests with an html response
-	// containing an element with class="a-winner-is-me".
-	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+self.addEventListener('fetch', function (event) {
+    // TODO: respond to all requests with an html response
+    // containing an element with class="a-winner-is-me".
+    // Ensure the Content-Type of the response is "text/html"
+
+    event.respondWith(
+        new Response('<b class="a-winner-is-me">Hello World!!</b>', {
+            headers: {'Content-Type': 'text/html'}
+        })
+    );
+
 });
