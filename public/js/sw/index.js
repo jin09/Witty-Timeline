f94d5bd809768fcd29277d866a2156904ca74665
Gautam Jain <supergautam995@gmail.com>
2018-01-23 00:40:51 +0530

    answer - serve the skeleton page instead of root page

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,4 +1,5 @@
-var staticCacheName = 'wittr-static-v3';
+var staticCacheName = 'wittr-static-v4';
+//changed cache name as we change the cache now
 
 self.addEventListener('install', function(event) {
   // TODO: cache /skeleton rather than the root page
@@ -6,7 +7,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(staticCacheName).then(function(cache) {
       return cache.addAll([
-        '/',
+        '/skeleton',
         'js/main.js',
         'css/main.css',
         'imgs/icon.png',
@@ -35,7 +36,13 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
   // TODO: respond to requests for the root page with
   // the page skeleton from the cache
-
+    var requestURL = new URL(event.request.url);
+    if(requestURL.origin === location.origin){
+        if(requestURL.pathname === '/'){
+            event.respondWith(caches.match('/skeleton'));
+            return;
+        }
+    }
   event.respondWith(
     caches.match(event.request).then(function(response) {
       return response || fetch(event.request);
